82fe6de8cff3ae0710dd4ba00ad30436f004c5ae
sowmiyasaravanan14 <sowmiyasaravanan2005@gmail.com>
2026-03-16 21:22:45 +0530

    Time: 4 ms (99.84%) | Memory: 46.1 MB (48.87%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int rev = 0;
+        int og = x;
+        while(x>0){
+            int digit = x % 10;
+            rev = rev * 10 +digit;
+            x = x/10;
+        }
+        if(og == rev){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
